31b028c4556c22fb3b10470380994cc4ec3f47ad
mdiktushar <md.tushar1804@gmail.com>
2024-03-24 17:47:04 +0600

    MainLayout & Routes Folder Setup

---
+++ src/routes/PublicRoutes.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "../layout/MainLayout/MainLayout";
+import Home from "../pages/public/Home";
+
+const PublicRoutes = createBrowserRouter([
+    {
+        path: `/`,
+        element: <MainLayout />,
+        children: [
+            {
+                path: `/`,
+                element: <Home />,
+            }
+        ]
+    }
+])
+
+export default PublicRoutes;
