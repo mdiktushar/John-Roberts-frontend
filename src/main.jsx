abb0b65c1e2fcad4fa72211c1749669a8aced6e0
mdiktushar <md.tushar1804@gmail.com>
2024-03-28 16:34:23 +0600

    folder structure for data

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import PublicRoutes from "./routes/PublicRoutes.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <div className="bg-[#f2f5f7]">
+    <div className="bg-[#f2f5f7] min-h-screen">
       <div className="max-w-screen-xl mx-auto">
         <RouterProvider router={PublicRoutes} />
       </div>
