79a5f24b54c688ab16288fef4e435356d0b225da
mdiktushar <md.tushar1804@gmail.com>
2024-04-08 17:59:20 +0600

    update subscribe

--- src/pages/public/shared/Subscribe/Subscribe.jsx
+++ src/pages/public/shared/Subscribe/Subscribe.jsx
@@ -1,24 +1,40 @@
 import React from "react";
 
 const Subscribe = () => {
+  const handleSubscribe = (event) => {
+    // Prevent the default behavior of form submission in React
+    event.preventDefault();
+    // Retrieve the value of the input field by name
+    console.log(event.target.elements.email.value);
+  };
+
   return (
     <div className="flex flex-col items-center gap-6 bg-white px-[30px] py-[100px] rounded">
-      <h2 className="text-center TimeNewRoman font-bold text-5xl text-gray-900">Be the first to kenw</h2>
+      <h2 className="text-center TimeNewRoman font-bold text-5xl text-gray-900">
+        Be the first to kenw
+      </h2>
       <p className="text-center text-sm max-w-[460px] text-gray-500">
         Lectus amet scelerisque fusce est venenatis, eget enim dolor amet vitae
         pharetra.
       </p>
-      <div className="flex flex-row gap-3 justify-center">
+      <form
+        onSubmit={handleSubscribe}
+        className="flex flex-row gap-3 justify-center"
+      >
         <input
-          type="email"
+          type="text"
+          name="email"
           placeholder="Your email please"
-          class="input input-bordered input-md w-xs md:w-[300px] lg:w-[400px]"
+          className="input input-bordered input-md w-xs md:w-[300px] lg:w-[400px]"
         />
 
-        <button class="btn btn-outline border-[#5f4ecb] text-[#5f4ecb] hover:text-white hover:bg-[#5f4ecb] px-6">
+        <button
+          type="submit"
+          className="btn btn-outline border-[#5f4ecb] text-[#5f4ecb] hover:text-white hover:bg-[#5f4ecb] px-6"
+        >
           Subscribe
         </button>
-      </div>
+      </form>
     </div>
   );
 };
