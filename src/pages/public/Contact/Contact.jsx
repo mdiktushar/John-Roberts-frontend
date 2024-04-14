b68285cedcbb9e47a615d2f4f4996ed624f7315f
mdiktushar <md.tushar1804@gmail.com>
2024-04-14 17:09:38 +0600

    Contact component #2

--- src/pages/public/Contact/Contact.jsx
+++ src/pages/public/Contact/Contact.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
+import TitleBanner from "../shared/TitleBanner/TitleBanner";
 
 const Contact = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const [banner, setBanner] = useState({
+    title: `Contact`,
+    description: `Porttitor in nibh id aliquet quam aliquam aliquet pulvinar integer dolor quis elementum, dui cursus nisi, nunc viverra nulla fringilla.`,
+  });
+  return (
+    <div>
+      {/** Passing the data to banner component  */}
+      <TitleBanner banner={banner} />
+
+
+      {/* Subscribe Component */}
+      <Subscribe />
+    </div>
+  );
 };
 
 export default Contact;
